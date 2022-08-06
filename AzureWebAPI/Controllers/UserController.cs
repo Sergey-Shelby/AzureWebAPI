48a07a7afe9d401d3400709cbe183c1a251a0828
Sergey <max.clansy@gmail.com>
2022-08-06 17:12:03 +0300

    Added user controller

---
+++ AzureWebAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using AzureWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AzureWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public User Get()
+        {
+            var user = new User()
+            {
+                Name = HttpContext.User.Claims.FirstOrDefault(x => x.Type == "name")?.Value,
+                Email = HttpContext.User.Claims.FirstOrDefault(x => x.Type == "preferred_username")?.Value
+            };
+
+            return user;
+        }
+    }
+}
---
+++ AzureWebAPI/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace AzureWebAPI.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
